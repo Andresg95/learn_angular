b52f8d3b39302e7a30f388c9d0c0e81d88eafd86
Andresg95 <andresgarcia.innova@gmail.com>
2021-02-16 12:29:27 +0100

    body and structural directives

--- my-app/src/app/components/body/body.component.ts
+++ my-app/src/app/components/body/body.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
     templateUrl: './body.component.html'
 })
 export class BodyComponent {
-
+    
+    show:boolean = true;
+    phrase : any = {
+        text: 'Great power carries great responsability',
+        author: 'Ben parker'
+    }
+    characters: string[] = ["Spiderman", "Venom", "Dr. Octopus"];
 
 }
