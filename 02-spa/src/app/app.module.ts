dbb3250eb73544325f206a35754793962f1c6de0
Andresg95 <andresgarcia.innova@gmail.com>
2021-02-20 00:37:49 +0100

    SPA heroes

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+//ROUTING
+import { APP_ROUTING } from "./app.routes"
+
+//SERVICES
+import { HeroesService } from './services/heroes.service';
+
+//COMPONENTS
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Hero } from '../../services/heroes.service';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:Hero[] = [];
+  constructor(private _heroesService:HeroesService) {
+
+   }
+
+  ngOnInit() {
+
+    this.heroes = this._heroesService.getHeroes();
+
+
+  }
+
+}
